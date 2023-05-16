b3893d9db828a9d9b73a9ad02b125151271e13a7
gyetvaigy93@gmail.com <gyetvaigy93@gmail.com>
2023-05-16 20:06:34 +0200

    cookingV0.5

--- aranyparszamolo/app.js
+++ aranyparszamolo/app.js
@@ -1,96 +1,101 @@
-//Alap programkód
-calulateButton.addEventListener("click", documentWrite);
-
-//Százalékszámító
+// Az elemek eléréséhez szükséges ID-k
+var selectId = 'hozzavalokEgy';
+var tableId = 'tabla';
+var createButtonId = 'tableCreateButton';
+// Az adatokat tartalmazó tömb
+var adatok = [
+    { value: '1', hozzavalo: 'Tojas' },
+    { value: '2', hozzavalo: 'Tej' },
+    { value: '3', hozzavalo: 'Cukor' },
+    { value: '4', hozzavalo: 'Piros Paprika' },
+    { value: '5', hozzavalo: 'Liszt' },
+    { value: '6', hozzavalo: 'Tejföl' },
+    { value: '7', hozzavalo: 'Tejszin' },
+    { value: '8', hozzavalo: 'Füszer' },
+    { value: '9', hozzavalo: 'Alaplé' },
+];
+// Random ID generálása
+function generateRandomId() {
+    return "input-".concat(Math.random().toString(36).substr(2, 9));
+}
 function szazalek() {
-    let ertekEgy = document.querySelector("#darabEgy").value
-    let ertekKetto = document.querySelector("#darabKetto").value
-    let calulate = (ertekEgy / 100);
-    let szazalek = (ertekKetto / calulate).toFixed(2);
-    return szazalek
+    var ertekEgy = document.querySelector("#darabEgy").value;
+    var ertekKetto = document.querySelector("#darabKetto").value;
+    var calulate = (ertekEgy / 100);
+    var szazalek = (ertekKetto / calulate).toFixed(2);
+    return szazalek;
 }
-
-//Ki irató függvény
-function documentWrite() {
-    if (szazalek() == "NaN") {
-        alert = ("Nem adtál meg értéket")
-    } else {
-        document.querySelector("#szazalakKi").innerHTML = "Kiszámolt érték : " + szazalek() + "%";
-    }
-
-    if (document.querySelector("#tojas").value == "") {
-        document.querySelector("#tojasKi").innerHTML = "-";
-    } else {
-        document.querySelector("#tojasKi").innerHTML = tojas() + " /db";
-    }
-    if (document.querySelector("#tevVizBe").value == "") {
-        document.querySelector("#tevVizKi").innerHTML = "-";
-    } else {
-        document.querySelector("#tevVizKi").innerHTML = tejViz() + " /ml";
-    }
-    if (document.querySelector("#kisKanalBe").value == "") {
-        document.querySelector("#kisKanalKi").innerHTML = "-";
-    } else {
-        document.querySelector("#kisKanalKi").innerHTML = kisKanal() + " /kk";
-    }
-    if (document.querySelector("#evoKanalBe").value == "") {
-        document.querySelector("#evoKanalKi").innerHTML = "-";
-    } else {
-        document.querySelector("#evoKanalKi").innerHTML = evoKanal() + " /ek";
-    }
-    if (document.querySelector("#liszt").value == "") {
-        document.querySelector("#lisztKi").innerHTML = "-";
-    } else {
-        document.querySelector("#lisztKi").innerHTML = liszt() + " /g";
-    }
-    if (document.querySelector("#csomag").value == "") {
-        document.querySelector("#csomagKi").innerHTML = "-";
-    } else {
-        document.querySelector("#csomagKi").innerHTML = csomag() + " /csomag";
-    }
-    if (document.querySelector("#tejfol").value == "") {
-        document.querySelector("#tejfolKi").innerHTML = "-";
-    } else {
-        document.querySelector("#tejfolKi").innerHTML = tejfolSz() + " /csomag";
-    }
-
-
+// Az adatok alapján táblázat generálása
+function generateTable(selectedValue) {
+    var _a;
+    var selectElement = document.getElementById(selectId);
+    var tableElement = document.getElementById(tableId);
+    // Táblázat sorainak száma
+    var rowCount = tableElement.rows.length;
+    // Új sor hozzáadása
+    var row = tableElement.insertRow(rowCount);
+    // Hozzávaló oszlop
+    var hozzavaloCell = row.insertCell();
+    hozzavaloCell.textContent = ((_a = adatok.find(function (item) { return item.value === selectedValue; })) === null || _a === void 0 ? void 0 : _a.hozzavalo) || ""; //ide kell egy figyelpt amit a Tej Tojas Valami
+    // Input mező oszlop
+    var inputCell = row.insertCell();
+    var inputId = generateRandomId();
+    var input = document.createElement('input');
+    input.type = 'text';
+    input.value = '';
+    input.id = inputId;
+    inputCell.appendChild(input);
+    // Számolás oszlop
+    var szamolasCell = row.insertCell();
+    szamolasCell.textContent = '';
+    // Eseménykezelő hozzáadása az input mezőhöz
+    input.addEventListener('input', function () {
+        var _a;
+        var nemTudom = (_a = adatok.find(function (item) { return item.value === selectedValue; })) === null || _a === void 0 ? void 0 : _a.hozzavalo;
+        var inputValue = Number(input.value);
+        var szamoltErtek = ((inputValue * szazalek()) / 100).toFixed(1);
+        if (nemTudom == "Tojas") {
+            szamolasCell.textContent = szamoltErtek.toString() + " /db";
+        }
+        ;
+        if (nemTudom == "Tej") {
+            szamolasCell.textContent = szamoltErtek.toString() + " /l";
+        }
+        ;
+        if (nemTudom == "Cukor") {
+            szamolasCell.textContent = szamoltErtek.toString() + " /kk";
+        }
+        ;
+        if (nemTudom == "Piros Paprika") {
+            szamolasCell.textContent = szamoltErtek.toString() + " /kk";
+        }
+        ;
+        if (nemTudom == "Liszta") {
+            szamolasCell.textContent = szamoltErtek.toString() + " /g";
+        }
+        ;
+        if (nemTudom == "Tejföl") {
+            szamolasCell.textContent = szamoltErtek.toString() + " /g";
+        }
+        ;
+        if (nemTudom == "Tejszin") {
+            szamolasCell.textContent = szamoltErtek.toString() + " /ml";
+        }
+        ;
+        if (nemTudom == "Füszer") {
+            szamolasCell.textContent = szamoltErtek.toString() + " /kk";
+        }
+        ;
+        if (nemTudom == "Alaplé") {
+            szamolasCell.textContent = szamoltErtek.toString() + " /dl";
+        }
+        ;
+    });
 }
-
-
-//További számoló függvények
-function tojas() {
-    let ertek = document.querySelector("#tojas").value;
-    let szamolas = ((ertek * szazalek()) / 100).toFixed(1);
-    return szamolas
-}
-
-function tejViz() {
-    let ertek = document.querySelector("#tevVizBe").value;
-    let szamolas = ((ertek * szazalek()) / 100).toFixed(1);
-    return szamolas
-}
-function kisKanal() {
-    let ertek = document.querySelector("#kisKanalBe").value;
-    let szamolas = ((ertek * szazalek()) / 100).toFixed(1);
-    return szamolas
-}
-function evoKanal() {
-    let ertek = document.querySelector("#evoKanalBe").value;
-    let szamolas = ((ertek * szazalek()) / 100).toFixed(1);
-    return szamolas
-} function liszt() {
-    let ertek = document.querySelector("#liszt").value;
-    let szamolas = ((ertek * szazalek()) / 100).toFixed(1);
-    return szamolas
-}
-function csomag() {
-    let ertek = document.querySelector("#csomag").value;
-    let szamolas = ((ertek * szazalek()) / 100).toFixed(1);
-    return szamolas
-}
-function tejfolSz() {
-    let ertek = document.querySelector("#tejfol").value;
-    let szamolas = ((ertek * szazalek()) / 100).toFixed(1);
-    return szamolas
-}
+// Gomb eseménykezelője
+var createButton = document.getElementById(createButtonId);
+createButton.addEventListener('click', function () {
+    var selectElement = document.getElementById(selectId);
+    var selectedValue = selectElement.value;
+    generateTable(selectedValue);
+});
---
+++ aranyparszamolo/app.ts
@@ -0,0 +1,86 @@
+// Az elemek eléréséhez szükséges ID-k
+const selectId = 'hozzavalokEgy';
+const tableId = 'tabla';
+const createButtonId = 'tableCreateButton';
+
+// Az adatokat tartalmazó tömb
+const adatok = [
+    { value: '1', hozzavalo: 'Tojas' },
+    { value: '2', hozzavalo: 'Tej' },
+    { value: '3', hozzavalo: 'Cukor' },
+    { value: '4', hozzavalo: 'Piros Paprika' },
+    { value: '5', hozzavalo: 'Liszt' },
+    { value: '6', hozzavalo: 'Tejföl' },
+    { value: '7', hozzavalo: 'Tejszin' },
+    { value: '8', hozzavalo: 'Füszer' },
+    { value: '9', hozzavalo: 'Alaplé' },
+
+];
+
+// Random ID generálása
+function generateRandomId(): string {
+    return `input-${Math.random().toString(36).substr(2, 9)}`;
+}
+function szazalek(): any {
+    let ertekEgy = document.querySelector("#darabEgy").value;
+    let ertekKetto = document.querySelector("#darabKetto").value;
+    let calulate = (ertekEgy / 100);
+    let szazalek = (ertekKetto / calulate).toFixed(2);
+    return szazalek
+}
+// Az adatok alapján táblázat generálása
+function generateTable(selectedValue: string): void {
+    const selectElement = document.getElementById(selectId) as HTMLSelectElement;
+    const tableElement = document.getElementById(tableId) as HTMLTableElement;
+
+    // Táblázat sorainak száma
+    const rowCount = tableElement.rows.length;
+
+    // Új sor hozzáadása
+    const row = tableElement.insertRow(rowCount);
+
+    // Hozzávaló oszlop
+    const hozzavaloCell = row.insertCell();
+    hozzavaloCell.textContent = adatok.find((item) => item.value === selectedValue)?.hozzavalo || ""; //ide kell egy figyelpt amit a Tej Tojas Valami
+
+    // Input mező oszlop
+    const inputCell = row.insertCell();
+    const inputId = generateRandomId();
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.value = '';
+    input.id = inputId;
+    inputCell.appendChild(input);
+
+    // Számolás oszlop
+    const szamolasCell = row.insertCell();
+    szamolasCell.textContent = '';
+
+    // Eseménykezelő hozzáadása az input mezőhöz
+    input.addEventListener('input', () => {
+        const nemTudom = adatok.find((item) => item.value === selectedValue)?.hozzavalo;
+        const inputValue = Number(input.value);
+        const szamoltErtek = ((inputValue * szazalek()) / 100).toFixed(1);
+        if (nemTudom == "Tojas") { szamolasCell.textContent = szamoltErtek.toString() + " /db" };
+        if (nemTudom == "Tej") { szamolasCell.textContent = szamoltErtek.toString() + " /l" };
+        if (nemTudom == "Cukor") { szamolasCell.textContent = szamoltErtek.toString() + " /kk" };
+        if (nemTudom == "Piros Paprika") { szamolasCell.textContent = szamoltErtek.toString() + " /kk" };
+        if (nemTudom == "Liszta") { szamolasCell.textContent = szamoltErtek.toString() + " /g" };
+        if (nemTudom == "Tejföl") { szamolasCell.textContent = szamoltErtek.toString() + " /g" };
+        if (nemTudom == "Tejszin") { szamolasCell.textContent = szamoltErtek.toString() + " /ml" };
+        if (nemTudom == "Füszer") { szamolasCell.textContent = szamoltErtek.toString() + " /kk" };
+        if (nemTudom == "Alaplé") { szamolasCell.textContent = szamoltErtek.toString() + " /dl" };
+
+    });
+}
+
+// Gomb eseménykezelője
+const createButton = document.getElementById(createButtonId) as HTMLButtonElement;
+createButton.addEventListener('click', () => {
+    const selectElement = document.getElementById(selectId) as HTMLSelectElement;
+    const selectedValue = selectElement.value;
+
+    generateTable(selectedValue);
+});
+
+
